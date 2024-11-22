a26be14364ac665e0da861dc79cb43c8d93056fc
JUNHAN ZHANG <144378024+Clearsky21z@users.noreply.github.com>
2024-11-22 15:43:10 -0500

    Updated model

--- scripts/02-clean_data.R
+++ scripts/02-clean_data.R
@@ -10,11 +10,27 @@
 #### Workspace setup ####
 library(tidyverse)
 
+melb_data <- read.csv("/Users/clearsky21/Housing_Price_Analysis/data/01-raw_data/melb_data.csv")
+
 # Load the dataset and remove rows with missing values
 melb_data_clean <- na.omit(melb_data)
+
+# Define the current year
 current_year <- 2024
+
+# Add BuildingAge and filter out rows with non-positive values
 melb_data_clean <- melb_data_clean %>%
-  mutate(BuildingAge = ifelse(!is.na(YearBuilt), current_year - YearBuilt, 0))
+  mutate(
+    BuildingAge = ifelse(!is.na(YearBuilt), current_year - YearBuilt, 0)
+  ) %>%
+  filter(
+    Distance > 0,
+    Landsize > 0,
+    BuildingArea > 0,
+    Price > 0,
+    Propertycount > 0,
+  )
 
 #### Save data ####
 write_csv(melb_data_clean, "data/02-analysis_data/cleaned_data.csv")
+
